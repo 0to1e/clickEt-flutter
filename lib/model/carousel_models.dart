53696da2adf7d96a238dc29ca912da082ba88122
Rohan Chaulagain <86584369+r0hanchaulagain@users.noreply.github.com>
2024-12-20 20:38:42 +0545

    coursel data class definition

---
+++ lib/model/carousel_models.dart
@@ -0,0 +1,8 @@
+class Movie {
+  final String title;
+  final String rating;
+  final String category;
+  final String imageUrl;
+
+  Movie({required this.title, required this.rating, required this.category, required this.imageUrl});
+}
