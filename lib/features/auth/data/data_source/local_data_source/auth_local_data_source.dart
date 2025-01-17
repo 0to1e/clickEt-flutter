b7c2480455a5ff1f436f9f23eb017cca8d67e246
Rohan Chaulagain <86584369+r0hanchaulagain@users.noreply.github.com>
2025-01-17 21:41:02 +0545

    data and domain layer refactor for login and sign up pages

---
+++ lib/features/auth/data/data_source/local_data_source/auth_local_data_source.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:ClickEt/features/auth/data/data_source/auth_data_source.dart';
+import 'package:ClickEt/features/auth/data/model/auth_hive_model.dart';
+import 'package:ClickEt/features/auth/domain/entity/auth_entity.dart';
+import 'package:ClickEt/network/hive_service.dart';
+
+class AuthLocalDataSource implements IAuthDataSource {
+  final HiveService _hiveService;
+
+  AuthLocalDataSource(this._hiveService);
+
+  @override
+  Future<AuthEntity> getCurrentUser() {
+    return Future.value(const AuthEntity(
+      userId: "",
+      fullName: "",
+      userName: "",
+      email: '',
+      phoneNumber: "",
+      profileURL: "",
+      password: "",
+    ));
+  }
+
+  @override
+  Future<String> loginStudent(String username, String password) async {
+    try {
+      await _hiveService.login(username, password);
+      return Future.value("Success");
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<void> registerStudent(AuthEntity student) async {
+    try {
+      // Convert AuthEntity to AuthHiveModel
+      final authHiveModel = AuthHiveModel.fromEntity(student);
+
+      await _hiveService.register(authHiveModel);
+      return Future.value();
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+
+  @override
+  Future<String> uploadProfilePicture(File file) {
+    throw UnimplementedError();
+  }
+}
