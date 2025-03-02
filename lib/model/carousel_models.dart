711c1408dd13412b9e3d342298b02394976cc97b
RohaN <166110329+0to1e@users.noreply.github.com>
2025-03-03 01:25:23 +0545

    refactoring codebase

--- lib/model/carousel_models.dart
+++
@@ -1,8 +0,0 @@
-class Movie {
-  final String title;
-  final String rating;
-  final String category;
-  final String imageUrl;
-
-  Movie({required this.title, required this.rating, required this.category, required this.imageUrl});
-}
