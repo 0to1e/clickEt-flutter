6fe0441ee273fc7098768db7a698920e3c3f33f0
RohaN <166110329+0to1e@users.noreply.github.com>
2025-03-03 01:22:34 +0545

    updated bloc injection

--- lib/features/auth/presentation/view_model/login/login_bloc.dart
+++ lib/features/auth/presentation/view_model/login/login_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:ClickEt/app/di/di.dart';
+import 'package:ClickEt/common/main_screen.dart';
 import 'package:ClickEt/features/auth/presentation/view/registration_view.dart';
 import 'package:ClickEt/features/auth/presentation/view_model/register/register_bloc.dart';
 import 'package:equatable/equatable.dart';
@@ -6,7 +7,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:ClickEt/features/auth/domain/use_case/login_use_case.dart';
 import 'package:ClickEt/features/movie/presentation/view_model/movie_bloc.dart';
-import 'package:ClickEt/features/home/presentation/view/home_view.dart';
 import 'package:ClickEt/network/hive_service.dart';
 import 'package:ClickEt/core/sensor/shake_cubit.dart';
 
@@ -31,7 +31,8 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     on<NavigateToRegisterEvent>(_onNavigateToRegister);
   }
 
-  void _onLoginSubmitted(LoginSubmittedEvent event, Emitter<LoginState> emit) async {
+  void _onLoginSubmitted(
+      LoginSubmittedEvent event, Emitter<LoginState> emit) async {
     // Validate login form
     final validationError = _validateLoginForm(
       usernameOrEmail: event.username,
@@ -59,14 +60,13 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         );
       },
       (response) {
-        emit(state.copyWith(isLoading: false, isSuccess: true, isLoggedIn: true));
-        shakeCubit.startListening(); // ✅ Start shake detection globally
+        emit(state.copyWith(
+            isLoading: false, isSuccess: true, isLoggedIn: true));
+        shakeCubit.startListening();
+
         Navigator.pushReplacement(
           event.context,
-          MaterialPageRoute(
-            builder: (context) =>
-                BlocProvider.value(value: movieBloc, child: const HomeView()),
-          ),
+          MaterialPageRoute(builder: (context) => const MainScreen()),
         );
       },
     );
@@ -75,18 +75,20 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   void _onLogout(LogOutEvent event, Emitter<LoginState> emit) async {
     try {
       await hiveService.deleteCurrentUser();
-      shakeCubit.stopListening(); 
+      shakeCubit.stopListening();
       emit(state.copyWith(isLoggedIn: false));
     } catch (e) {
       emit(state.copyWith(isLoggedIn: true));
     }
   }
 
-  void _onTogglePasswordVisibility(TogglePasswordVisibilityEvent event, Emitter<LoginState> emit) {
+  void _onTogglePasswordVisibility(
+      TogglePasswordVisibilityEvent event, Emitter<LoginState> emit) {
     emit(state.copyWith(isPasswordVisible: !state.isPasswordVisible));
   }
 
-  void _onNavigateToRegister(NavigateToRegisterEvent event, Emitter<LoginState> emit) {
+  void _onNavigateToRegister(
+      NavigateToRegisterEvent event, Emitter<LoginState> emit) {
     Navigator.pushReplacement(
       event.context,
       MaterialPageRoute(
@@ -98,12 +100,13 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     );
   }
 
-    void navigateToRegister(BuildContext context) {
+  void navigateToRegister(BuildContext context) {
     add(NavigateToRegisterEvent(context: context));
   }
 
   // Validation logic for login form
-  String? _validateLoginForm({required String usernameOrEmail, required String password}) {
+  String? _validateLoginForm(
+      {required String usernameOrEmail, required String password}) {
     if (usernameOrEmail.isEmpty || password.isEmpty) {
       return "Username/Email and password are required";
     }
