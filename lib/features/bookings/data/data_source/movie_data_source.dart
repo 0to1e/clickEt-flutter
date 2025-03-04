0d122a8ca7c68ad41285bb0c6b9ef083f8716834
RohaN <166110329+0to1e@users.noreply.github.com>
2025-03-04 15:14:19 +0545

    updated booking repository

--- lib/features/bookings/data/data_source/movie_data_source.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ClickEt/features/movie/domain/entity/movie_entity.dart';
-
-abstract interface class IMovieDataSource {
-  Future<List<MovieEntity>> getShowingMovies();
-  Future<List<MovieEntity>> getUpcomingMovies();
-  Future<void> cacheMovies(List<MovieEntity> movies);
-  Future <List<MovieEntity>> getCachedShowingMovies();
-  Future<List<MovieEntity>> getCachedUpcomingMovies();
-}
