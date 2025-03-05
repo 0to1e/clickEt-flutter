e646c71e63e9bb18e8fe46224b5b475e7410f003
RohaN <166110329+0to1e@users.noreply.github.com>
2025-03-05 15:29:43 +0545

    login bloc update post tests

--- lib/features/auth/presentation/view_model/login/login_state.dart
+++ lib/features/auth/presentation/view_model/login/login_state.dart
@@ -5,12 +5,18 @@ class LoginState extends Equatable {
   final bool isLoading;
   final bool isSuccess;
   final bool isPasswordVisible;
+  final String? errorMessage; 
+  final bool navigateToMain; 
+  final bool navigateToRegister;
 
   const LoginState({
     this.isLoggedIn = false,
     this.isLoading = false,
     this.isSuccess = false,
     this.isPasswordVisible = false,
+    this.errorMessage,
+    this.navigateToMain = false,
+    this.navigateToRegister = false,
   });
 
   factory LoginState.initial() => const LoginState();
@@ -20,15 +26,29 @@ class LoginState extends Equatable {
     bool? isLoading,
     bool? isSuccess,
     bool? isPasswordVisible,
+    String? errorMessage,
+    bool? navigateToMain,
+    bool? navigateToRegister,
   }) {
     return LoginState(
       isLoggedIn: isLoggedIn ?? this.isLoggedIn,
       isLoading: isLoading ?? this.isLoading,
       isSuccess: isSuccess ?? this.isSuccess,
       isPasswordVisible: isPasswordVisible ?? this.isPasswordVisible,
+      errorMessage: errorMessage,
+      navigateToMain: navigateToMain ?? this.navigateToMain,
+      navigateToRegister: navigateToRegister ?? this.navigateToRegister,
     );
   }
 
   @override
-  List<Object> get props => [isLoggedIn, isLoading, isSuccess, isPasswordVisible];
+  List<Object?> get props => [
+        isLoggedIn,
+        isLoading,
+        isSuccess,
+        isPasswordVisible,
+        errorMessage,
+        navigateToMain,
+        navigateToRegister,
+      ];
 }
